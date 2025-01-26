d59a0c2cddb6d0ddc52ea7d5d6a62fa1e4d52c30
Robert Coltheart <13191652+robertcoltheart@users.noreply.github.com>
2025-01-26 22:05:30 +1100

    Add boolean tests (#5)
    * Add boolean tests
    
    * executable

---
+++ src/Shouldst.Tests/BooleanTests.cs
@@ -0,0 +1,70 @@
+﻿namespace Shouldst.Tests;
+
+public class BooleanTests
+{
+    [Test]
+    public void AssertTrueWithTrueSucceeds()
+    {
+        true.ShouldBeTrue();
+    }
+
+    [Test]
+    public void AsserTrueWithFalseThrows()
+    {
+        Assert.Throws<ShouldException>(() => false.ShouldBeTrue());
+    }
+
+    [Test]
+    public void AssertFalseWithFalseSucceeds()
+    {
+        false.ShouldBeFalse();
+    }
+
+    [Test]
+    public void AssertFalseWithTrueThrows()
+    {
+        Assert.Throws<ShouldException>(() => true.ShouldBeFalse());
+    }
+
+    [Test]
+    [Arguments(true)]
+    public void AssertTrueWithNullableTrueSucceeds(bool? value)
+    {
+        value.ShouldBeTrue();
+    }
+
+    [Test]
+    [Arguments(null)]
+    public void AssertTrueWithNullThrows(bool? value)
+    {
+        Assert.Throws<ShouldException>(() => value.ShouldBeTrue());
+    }
+
+    [Test]
+    [Arguments(false)]
+    public void AssertTrueWithNullableFalseThrows(bool? value)
+    {
+        Assert.Throws<ShouldException>(() => value.ShouldBeTrue());
+    }
+
+    [Test]
+    [Arguments(true)]
+    public void AssertFalseWithNullableTrueThrows(bool? value)
+    {
+        Assert.Throws<ShouldException>(() => value.ShouldBeFalse());
+    }
+
+    [Test]
+    [Arguments(null)]
+    public void AssertFalseWithNullThrows(bool? value)
+    {
+        Assert.Throws<ShouldException>(() => value.ShouldBeFalse());
+    }
+
+    [Test]
+    [Arguments(false)]
+    public void AssertFalseWithNullableFalseSucceeds(bool? value)
+    {
+        value.ShouldBeFalse();
+    }
+}
