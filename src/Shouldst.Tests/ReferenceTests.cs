0883972110fd52191f335cdd0c100825d7e7f19f
Robert Coltheart <robert.coltheart@gmail.com>
2025-01-27 21:11:30 +1100

    Add reference tests

---
+++ src/Shouldst.Tests/ReferenceTests.cs
@@ -0,0 +1,36 @@
+﻿namespace Shouldst.Tests;
+
+public class ReferenceTests
+{
+    [Test]
+    public void AssertSameWithSameSucceeds()
+    {
+        var value = new object();
+
+        value.ShouldBeTheSameAs(value);
+    }
+
+    [Test]
+    public void AssertSameWithDifferentThrows()
+    {
+        var value = new object();
+
+        Assert.Throws<ShouldException>(() => value.ShouldBeTheSameAs(new object()));
+    }
+
+    [Test]
+    public void AssertDifferentWithSameThrows()
+    {
+        var value = new object();
+
+        Assert.Throws<ShouldException>(() => value.ShouldNotBeTheSameAs(value));
+    }
+
+    [Test]
+    public void AssertDifferentWithDifferentSucceeds()
+    {
+        var value = new object();
+
+        value.ShouldNotBeTheSameAs(new object());
+    }
+}
