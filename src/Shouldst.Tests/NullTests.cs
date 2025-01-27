2ccf6c87754127b9dd507bb488434971d75c2925
Robert Coltheart <robert.coltheart@gmail.com>
2025-01-27 20:28:05 +1100

    Add null tests

---
+++ src/Shouldst.Tests/NullTests.cs
@@ -0,0 +1,36 @@
+﻿namespace Shouldst.Tests;
+
+public class NullTests
+{
+    [Test]
+    public void AssertNullWithNullSucceeds()
+    {
+        var value = default(object);
+
+        value.ShouldBeNull();
+    }
+
+    [Test]
+    public void AssertNullWithNotNullThrows()
+    {
+        var value = new object();
+
+        Assert.Throws<ShouldException>(() => value.ShouldBeNull());
+    }
+
+    [Test]
+    public void AssertNotNullWithNullThrows()
+    {
+        var value = default(object);
+
+        Assert.Throws<ShouldException>(() => value.ShouldNotBeNull());
+    }
+
+    [Test]
+    public void AssertNotNullWithNotNullSucceeds()
+    {
+        var value = new object();
+
+        value.ShouldNotBeNull();
+    }
+}
--- src/Shouldst/Comparers/ComparerFactory.cs
+++ src/Shouldst/Comparers/ComparerFactory.cs
@@ -4,14 +4,14 @@ internal static class ComparerFactory
 {
     public static IEnumerable<ICompareStrategy<T>> GetComparers<T>()
     {
-        return new ICompareStrategy<T>[]
-        {
+        return
+        [
             new EnumerableComparer<T>(),
             new GenericTypeComparer<T>(),
             new ComparableComparer<T>(),
             new EquatableComparer<T>(),
             new TypeComparer<T>()
-        };
+        ];
     }
 
     public static IComparer<T> GetDefaultComparer<T>()
