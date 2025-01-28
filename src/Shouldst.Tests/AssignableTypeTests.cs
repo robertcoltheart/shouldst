9dbbd2878d6a0efdd88d20c65d974f695cfdc6be
Robert Coltheart <13191652+robertcoltheart@users.noreply.github.com>
2025-01-29 09:04:10 +1100

    Add assignable to tests (#14)

---
+++ src/Shouldst.Tests/AssignableTypeTests.cs
@@ -0,0 +1,84 @@
+﻿namespace Shouldst.Tests;
+
+public class AssignableTypeTests
+{
+    [Test]
+    public void AssertAssignableTypeWithSameSucceeds()
+    {
+        var value = new MyType();
+
+        value.ShouldBeAssignableTo<MyType>();
+        value.ShouldBeAssignableTo(typeof(MyType));
+    }
+
+    [Test]
+    public void AssertNotAssignableTypeWithSameThrows()
+    {
+        var value = new MyType();
+
+        Assert.Throws<ShouldException>(() => value.ShouldNotBeAssignableTo<MyType>());
+        Assert.Throws<ShouldException>(() => value.ShouldNotBeAssignableTo(typeof(MyType)));
+    }
+
+    [Test]
+    public void AssertAssignableTypeWithDifferentThrows()
+    {
+        var value = new MyType();
+
+        Assert.Throws<ShouldException>(() => value.ShouldBeAssignableTo<IsolatedType>());
+        Assert.Throws<ShouldException>(() => value.ShouldBeAssignableTo(typeof(IsolatedType)));
+    }
+
+    [Test]
+    public void AssertNotAssignableTypeWithDifferentSucceeds()
+    {
+        var value = new MyType();
+
+        value.ShouldNotBeAssignableTo<IsolatedType>();
+        value.ShouldNotBeAssignableTo(typeof(IsolatedType));
+    }
+
+    [Test]
+    public void AssertAssignableTypeWithDerivedSucceeds()
+    {
+        var value = new MyDerivedType();
+
+        value.ShouldBeAssignableTo<MyType>();
+        value.ShouldBeAssignableTo(typeof(MyType));
+    }
+
+    [Test]
+    public void AssertAssignableTypeWithImplementedSucceeds()
+    {
+        var value = new MyDerivedType();
+
+        value.ShouldBeAssignableTo<IMyInterface>();
+        value.ShouldBeAssignableTo(typeof(IMyInterface));
+    }
+
+    [Test]
+    public void AssertNotAssignableTypeWithDerivedThrows()
+    {
+        var value = new MyDerivedType();
+
+        Assert.Throws<ShouldException>(() => value.ShouldNotBeAssignableTo<MyType>());
+        Assert.Throws<ShouldException>(() => value.ShouldNotBeAssignableTo(typeof(MyType)));
+    }
+
+    [Test]
+    public void AssertNotAssignableTypeWithImplementedThrows()
+    {
+        var value = new MyDerivedType();
+
+        Assert.Throws<ShouldException>(() => value.ShouldNotBeAssignableTo<IMyInterface>());
+        Assert.Throws<ShouldException>(() => value.ShouldNotBeAssignableTo(typeof(IMyInterface)));
+    }
+
+    private class MyType;
+
+    private class MyDerivedType : MyType, IMyInterface;
+
+    private class IsolatedType;
+
+    private interface IMyInterface;
+}
