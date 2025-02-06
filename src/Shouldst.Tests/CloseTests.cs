4d8b8a72d7b3b906e0c2ff67fa6410844e4ff2be
Robert Coltheart <robert.coltheart@gmail.com>
2025-02-07 09:06:49 +1100

    Add empty tests

--- src/Shouldst.Tests/CloseTests.cs
+++ src/Shouldst.Tests/CloseTests.cs
@@ -24,6 +24,8 @@ public class CloseTests
         1f.ShouldBeCloseTo(1.1f, 0.15f);
         1d.ShouldBeCloseTo(1.1d, 0.15d);
         1m.ShouldBeCloseTo(1.1m, 0.15m);
+        TimeSpan.FromSeconds(1).ShouldBeCloseTo(TimeSpan.FromSeconds(1), TimeSpan.FromSeconds(0.1));
+        DateTime.Parse("2025-02-05T12:30:00Z").ShouldBeCloseTo(DateTime.Parse("2025-02-05T12:30:01Z"), TimeSpan.FromSeconds(5));
     }
 
     [Test]
@@ -32,5 +34,7 @@ public class CloseTests
         Assert.Throws<ShouldException>(() => 1f.ShouldBeCloseTo(2f, 0.15f));
         Assert.Throws<ShouldException>(() => 1d.ShouldBeCloseTo(2d, 0.15d));
         Assert.Throws<ShouldException>(() => 1m.ShouldBeCloseTo(2m, 0.15m));
+        Assert.Throws<ShouldException>(() => TimeSpan.FromSeconds(1).ShouldBeCloseTo(TimeSpan.FromSeconds(5), TimeSpan.FromSeconds(0.1)));
+        Assert.Throws<ShouldException>(() => DateTime.Parse("2025-02-05T12:30:00Z").ShouldBeCloseTo(DateTime.Parse("2025-02-05T12:50:01Z"), TimeSpan.FromSeconds(10)));
     }
 }
---
+++ src/Shouldst.Tests/EmptyTests.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using FluentAssertions;
+
+namespace Shouldst.Tests;
+
+public class EmptyTests
+{
+    [Test]
+    public void ShouldBeEmptyEnumerableWithEmptySucceeds()
+    {
+        GetEmptyValues().ShouldBeEmpty();
+        GetEmptyValuesNonGeneric().ShouldBeEmpty();
+    }
+
+    [Test]
+    public void ShouldNotBeEmptyEnumerableWithEmptyThrows()
+    {
+        Assert.Throws<ShouldException>(() => GetEmptyValues().ShouldNotBeEmpty());
+        Assert.Throws<ShouldException>(() => GetEmptyValuesNonGeneric().ShouldNotBeEmpty());
+    }
+
+    [Test]
+    public void ShouldBeEmptyEnumerableWithNonEmptyThrows()
+    {
+        Assert.Throws<ShouldException>(() => GetValues().ShouldBeEmpty());
+        Assert.Throws<ShouldException>(() => GetValuesNonGeneric().ShouldBeEmpty());
+    }
+
+    [Test]
+    public void ShouldNotBeEmptyEnumerableWithNonEmptySucceeds()
+    {
+        GetValues().ShouldNotBeEmpty();
+        GetValuesNonGeneric().ShouldNotBeEmpty();
+    }
+
+    [Test]
+    public void ShouldBeEmptyStringWithEmptySucceeds()
+    {
+        string.Empty.ShouldBeEmpty();
+    }
+
+    [Test]
+    public void ShouldNotBeEmptyWithEmptyThrows()
+    {
+        Assert.Throws<ShouldException>(() => string.Empty.ShouldNotBeEmpty());
+    }
+
+    [Test]
+    public void ShouldBeEmptyWithStringThrows()
+    {
+        Assert.Throws<ShouldException>(() => "val".ShouldBeEmpty());
+    }
+
+    [Test]
+    public void ShouldNotBeEmptyWithStringSucceeds()
+    {
+        "val".ShouldNotBeEmpty();
+    }
+
+    private IEnumerable<string> GetValues()
+    {
+        yield return "1";
+        yield return "2";
+    }
+
+    private IEnumerable GetValuesNonGeneric()
+    {
+        return new[]
+        {
+            "1",
+            "2"
+        };
+    }
+
+    private IEnumerable<string> GetEmptyValues()
+    {
+        yield break;
+    }
+
+    private IEnumerable GetEmptyValuesNonGeneric()
+    {
+        return Array.Empty<string>();
+    }
+}
