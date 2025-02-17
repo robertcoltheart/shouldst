97543b33badb97688c9638a71bce315f3e9cc420
Robert Coltheart <13191652+robertcoltheart@users.noreply.github.com>
2025-02-18 08:28:32 +1100

    Add exception tests (#34)

---
+++ src/Shouldst.Tests/ExceptionTests.cs
@@ -0,0 +1,16 @@
+﻿namespace Shouldst.Tests;
+
+public class ExceptionTests
+{
+    [Test]
+    public void AssertExceptionContainsMessageWithMatchingSucceeds()
+    {
+        new Exception("error message").ShouldContainErrorMessage(" message");
+    }
+
+    [Test]
+    public void AssertExceptionContainsMessageWithMissingThrows()
+    {
+        Assert.Throws<ShouldException>(() => new Exception("error message").ShouldContainErrorMessage("missing"));
+    }
+}
